c9ad5c0bcec4443f059104af3f7549b08129fec8
Isteyaque khan <99346927+ishukhan@users.noreply.github.com>
2023-09-15 11:43:37 +0000

    data type summary

---
+++ javaScript/02Summary.js
@@ -0,0 +1,11 @@
+// Diffrent type of Data type?
+
+// primitive
+
+// 7 types : String, Number, Boolean, null, undifined, Symbol, BigInt
+
+ 
+
+// Reference type : (Non primitive)
+
+// Array, Objects, Functions

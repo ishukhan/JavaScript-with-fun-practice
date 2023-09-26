ccebde010481f45861b0a12fe7e0500d47cb3b6d
Isteyaque khan <99346927+ishukhan@users.noreply.github.com>
2023-09-26 12:46:33 +0000

    for loop

---
+++ javaScript/loops/01.js
@@ -0,0 +1,37 @@
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     const element = index;
+//     console.log(element)
+// }
+
+// console.log(element); error
+
+// nested loop
+
+// for (let i = 0; i <=5; i++) {
+//     console.log(`Outer loop ${i}`);
+//     for (let j = 0; j <=5; j++) {
+//         console.log(`Inner loop ${j}`);   
+//     }
+// }
+
+
+// for (let i = 1; i <=10; i++) {
+//     for (let j = 1; j <=10 ; j++) {    
+//         console.log(i +" "+ '*'+ " " + j + ' = ' + i*j);
+//     }    
+// }
+
+
+
+
+let myArry =["superman", "batman", "flash"]
+
+for (let index = 0; index < myArry.length; index++) {
+    const element = myArry[index];
+    console.log(`value of ${element}`)
+}

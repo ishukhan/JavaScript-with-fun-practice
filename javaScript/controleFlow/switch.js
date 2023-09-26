6fa01a0bf12a51e0aa22a8e7cb9464c38dff331d
Isteyaque khan <99346927+ishukhan@users.noreply.github.com>
2023-09-26 11:25:12 +0000

    switch statement

---
+++ javaScript/controleFlow/switch.js
@@ -0,0 +1,25 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3;
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Febuary")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    default:
+        console.log("default case match")
+        break;
+}

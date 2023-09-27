a3c07695ec7e74d205431f65e3089ff390f184d3
Isteyaque khan <99346927+ishukhan@users.noreply.github.com>
2023-09-27 08:38:18 +0000

    diff use of forEach

--- javaScript/loops/forEach.js
+++ javaScript/loops/forEach.js
@@ -39,5 +39,33 @@ const myCoding=[
 ]
 
 myCoding.forEach( (item) => {
-console.log(item.languageName)
+// console.log(item.languageName)
 })
+
+
+// store forEach in variable
+
+// const value = coding.forEach( (item) => {
+//     // console.log(item)
+//     return item
+// })
+// console.log(value)   // give undifiend   // forEach does'nt return value
+
+const myNum = [1,2,3,4,5,6,7,8,9]
+
+// const newNum = myNum.filter( (num) => num > 4)
+// const newNum = myNum.filter( (num) => {   //whenever use scope give return keyword
+//     return num > 4
+// })
+
+// const newNum = [];
+
+// myNum.forEach( (num) => {
+//     if(num>2){
+//         newNum.push(num)
+//     }
+// })
+
+
+// console.log(newNum)
+

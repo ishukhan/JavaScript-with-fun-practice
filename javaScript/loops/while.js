1cfa3cf98385fdcfa03b08a389a6e00d3ad82d1c
Isteyaque khan <99346927+ishukhan@users.noreply.github.com>
2023-09-27 06:10:39 +0000

    while loop

---
+++ javaScript/loops/while.js
@@ -0,0 +1,43 @@
+// while (condition) {
+    
+// }
+
+// let score =1
+// while (score<=10) {
+//     // console.log(`value of ${score}`)
+//     score++
+// }
+
+let myArry =["superman", "batan", "flash"]
+
+let arr =0;
+while(arr<myArry.length){
+    // console.log(`value of ${myArry[arr]}`)
+    arr++
+}
+
+// break or continue
+
+// let num =1;
+// while(num<=10){
+//     if(num==5){
+//         console.log(`Detacted 5`)
+//         break;
+//     }
+//     console.log(`value of num is ${num}`)
+//     num++
+// }
+
+
+// some error fix it
+// let i =1;
+// while(i<=10){
+//     if(i==5){
+//         console.log(`Detacted 5`)
+//         continue;
+//     }
+//     console.log(`value of i is ${i}`)
+//     i++
+// }
+
+

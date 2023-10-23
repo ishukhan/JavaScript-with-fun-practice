8d90434bfc7cb3c2668ef9a09fbc2b21b722c79e
ishukhan <ishukk00@gmail.com>
2023-10-23 12:45:54 +0530

    hex color generate

---
+++ javaScript/projects/Color Fliper/hex.js
@@ -0,0 +1,19 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  let hexColor = "#";
+  for (let i = 0; i < 6; i++) {
+    hexColor += hex[getRandomeNum()];
+  }
+
+  document.body.style.backgroundColor = hexColor;
+  color.textContent = hexColor;
+});
+
+// get a randome number
+function getRandomeNum() {
+  return Math.floor(Math.random() * hex.length);
+}

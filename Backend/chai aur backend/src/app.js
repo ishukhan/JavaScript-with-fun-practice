1cf2f678c1feb81f48c81b509993b75952d03f62
ishukhan <ishukk00@gmail.com>
2024-02-14 15:14:46 +0530

    all setup configured

--- Backend/chai aur backend/src/app.js
+++ Backend/chai aur backend/src/app.js
@@ -17,4 +17,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookiesParse());
 
+// routes import
+import  userRoute  from "./routes/user.route.js";
+
+// routes declare
+app.use("/api/v1/users", userRoute);
+
 export { app };

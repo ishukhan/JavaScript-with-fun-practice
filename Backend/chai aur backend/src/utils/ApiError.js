e7ad4c54c3926b56fb46eb334eab304f67cdf20e
ishukhan <ishukk00@gmail.com>
2024-02-15 13:14:02 +0530

    bug fix

--- Backend/chai aur backend/src/utils/ApiError.js
+++ Backend/chai aur backend/src/utils/ApiError.js
@@ -1,4 +1,4 @@
-class apiError extends Error{
+class ApiError extends Error{
     constructor(
         statusCode,
         message="Something went wrong",
@@ -19,3 +19,5 @@ class apiError extends Error{
         }
     }
 }
+
+export {ApiError}

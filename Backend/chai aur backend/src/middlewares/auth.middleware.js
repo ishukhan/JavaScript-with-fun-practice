6581e8c4fecafc9f37f7d488bb157144987b0a61
ishukhan <ishukk00@gmail.com>
2024-02-17 12:42:18 +0530

    bug fix

---
+++ Backend/chai aur backend/src/middlewares/auth.middleware.js
@@ -0,0 +1,35 @@
+import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import jwt from "jsonwebtoken";
+
+export const verifyJwt = asyncHandler(async (req, res, next) => {
+  try {
+    const token =
+      req.cookies.accessToken ||
+      req.header("Authorization")?.replace("Bearer ", "");
+
+      console.log(token)
+    if (!token) {
+      throw new ApiError(401, "Unauthorized request");
+    }
+
+    // if token available verify using jwt
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    // find the user
+    const user = await User.findById(decodedToken?._id).select(
+      "-password -refreshToken"
+    );
+
+    if (!user) {
+      //discuss about forntend
+      throw new ApiError(401, "Invalid access Token :)");
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    throw new ApiError(401, error?.message || "Invalid Access Token :)");
+  }
+});
